e7f80868507aa323fdadd027713d26ab8253b2e4
GCL <lewin.gc@gmail.com>
2018-01-21 13:15:17 -0500

    Initial commit

---
+++ imu.h
@@ -0,0 +1,115 @@
+#include <Wire.h>
+#include <SparkFunLSM9DS1.h>
+
+// SDO_XM and SDO_G are both pulled high, so our addresses are:
+#define LSM9DS1_M       0x1C
+#define LSM9DS1_AG      0x6A
+
+LSM9DS1 imu;
+
+void SetupGyro()
+{
+  // [enabled] turns the gyro on or off.
+  imu.settings.gyro.enabled = true;  // Enable the gyro
+  
+  // [scale] sets the full-scale range of the gyroscope.
+  // scale can be set to either 245, 500, or 2000
+  imu.settings.gyro.scale = 245; // Set scale to +/-245dps -- CONSULT TABLE 3 IN THE DATASHEET FOR ACTUAL SENSITIVITY!!!
+  
+  // [sampleRate] sets the output data rate (ODR) of the gyro
+  // sampleRate can be set between 1-6
+  // 1 = 14.9    4 = 238
+  // 2 = 59.5    5 = 476
+  // 3 = 119     6 = 952
+  imu.settings.gyro.sampleRate = 6; 
+  
+  // [bandwidth] can set the cutoff frequency of the gyro.
+  // Allowed values: 0-3. Actual value of cutoff frequency
+  // depends on the sample rate. (Datasheet section 7.12)
+  imu.settings.gyro.bandwidth = 0;
+  
+  // [lowPowerEnable] turns low-power mode on or off.
+  imu.settings.gyro.lowPowerEnable = false; // LP mode off
+  
+  // [HPFEnable] enables or disables the high-pass filter
+  imu.settings.gyro.HPFEnable = true; // HPF disabled
+  
+  // [HPFCutoff] sets the HPF cutoff frequency (if enabled)
+  // Allowable values are 0-9. Value depends on ODR.
+  // (Datasheet section 7.14)
+  imu.settings.gyro.HPFCutoff = 1; // HPF cutoff = 4Hz
+  
+  // [flipX], [flipY], and [flipZ] are booleans that can
+  // automatically switch the positive/negative orientation
+  // of the three gyro axes.
+  imu.settings.gyro.flipX = false; // Don't flip X
+  imu.settings.gyro.flipY = false; // Don't flip Y
+  imu.settings.gyro.flipZ = false; // Don't flip Z
+}
+
+void SetupAccel()
+{
+  // [enabled] turns the acclerometer on or off.
+  imu.settings.accel.enabled = true; // Enable accelerometer
+
+  // [enableX], [enableY], and [enableZ] can turn on or off
+  // select axes of the acclerometer.
+  imu.settings.accel.enableX = true; // Enable X
+  imu.settings.accel.enableY = true; // Enable Y
+  imu.settings.accel.enableZ = true; // Enable Z
+  
+  // [scale] sets the full-scale range of the accelerometer.
+  // accel scale can be 2, 4, 8, or 16
+  imu.settings.accel.scale = 16; // Set accel scale to +/-8g.
+  
+  // [sampleRate] sets the output data rate (ODR) of the
+  // accelerometer. ONLY APPLICABLE WHEN THE GYROSCOPE IS
+  // DISABLED! Otherwise accel sample rate = gyro sample rate.
+  // accel sample rate can be 1-6
+  // 1 = 10 Hz    4 = 238 Hz
+  // 2 = 50 Hz    5 = 476 Hz
+  // 3 = 119 Hz   6 = 952 Hz
+  imu.settings.accel.sampleRate = 1; // Set accel to 10Hz.
+  
+  // [bandwidth] sets the anti-aliasing filter bandwidth.
+  // Accel cutoff freqeuncy can be any value between -1 - 3. 
+  // -1 = bandwidth determined by sample rate
+  // 0 = 408 Hz   2 = 105 Hz
+  // 1 = 211 Hz   3 = 50 Hz
+  imu.settings.accel.bandwidth = 0; // BW = 408Hz
+  
+  // [highResEnable] enables or disables high resolution 
+  // mode for the acclerometer.
+  imu.settings.accel.highResEnable = false; // Disable HR
+  
+  // [highResBandwidth] sets the LP cutoff frequency of
+  // the accelerometer if it's in high-res mode.
+  // can be any value between 0-3
+  // LP cutoff is set to a factor of sample rate
+  // 0 = ODR/50    2 = ODR/9
+  // 1 = ODR/100   3 = ODR/400
+  imu.settings.accel.highResBandwidth = 0;  
+}
+
+int SetupIMU(void)
+{
+  Wire.begin();
+  // Before initializing the IMU, there are a few settings
+  // we may need to adjust. Use the settings struct to set
+  // the device's communication mode and addresses:
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+  
+  // The above lines will only take effect AFTER calling
+  // imu.begin(), which verifies communication with the IMU
+  // and turns it on.
+  
+  SetupAccel();
+  SetupGyro();
+
+  if(!imu.begin()) return 0;
+
+  return 1;
+}
+
---
+++ parachute_timer.ino
@@ -0,0 +1,189 @@
+#include "imu.h"
+#include <TinyScreen.h>
+
+//define the display
+TinyScreen display = TinyScreen(TinyScreenPlus);
+
+#define GRAVITY 1366 //with accelerometer set to  +-16g full scale => 0.732 mg/LSB => 1g = 1366
+
+//define the states of the system -- see state diagram
+enum ACCEL_STATE {RESETTING, STANDBY, READY, FREEFALL, CHUTE, LANDED};
+
+//threshold for detecting release
+#define FALL_THRESHOLD (GRAVITY / 3)
+
+//threshold for detecting landing
+#define LAND_THRESHOLD (5 * GRAVITY)  
+
+//threshold for detecting if the system is still before dropping
+#define WAIT_THRESHOLD ((GRAVITY * 7) / 8)  
+
+//length of time system must be "still" before dropping
+#define WAIT_TIME 3000
+
+//minimum flight time
+#define MIN_TIME 500 //minimum time for impact based on free fall -- don't want false positive when chute opens
+
+//start at resetting
+ACCEL_STATE state = RESETTING;
+
+uint32_t startTime = 0;
+uint32_t landTime = 0;
+
+#define  SERIAL_PORT_SPEED  115200
+
+#ifdef SERIAL_PORT_MONITOR
+  #define SerialMonitor SERIAL_PORT_MONITOR
+#else
+  #define SerialMonitor Serial
+#endif
+
+void setup()
+{
+  SerialMonitor.begin(115200);
+  //while (!SerialMonitor) {} // Uncomment this if you want to wait for the Serial monitor to open -- but then you must use it
+
+  SerialMonitor.println("Initializing..."); //let us know we're talking
+
+  //initialize the IMU
+  if (!SetupIMU()) //see imu.h for details of what this does
+  {
+    SerialMonitor.println("Failed to communicate with LSM9DS1.");
+    while (1) {}  //die gracefully
+  }
+
+  display.begin();
+  
+  //sets main current level, valid levels are 0-15
+  display.setBrightness(10);
+
+  startTime = millis();
+  
+  SerialMonitor.println("Done initializing.");
+}
+
+void loop(void)
+{
+  bool input = false;
+  if(SerialMonitor.available())
+  {
+    // Received a message
+    SerialMonitor.read();
+    input = true;
+  }
+
+  //button state machine
+  static int prevState = 1;
+  int currState = display.getButtons();
+  
+  if(prevState && !currState)
+  {
+    input = true;
+  }
+
+  prevState = currState;
+
+  if(input) //button has been pressed
+  {
+    if(state == RESETTING || state == LANDED)
+    {
+      state = STANDBY;
+      startTime = millis();
+      String message("\nPlease wait ");
+      message += String(WAIT_TIME / 1000);
+      message += (" seconds before dropping.\n");
+      PrintScreen(message);
+    }
+
+    else 
+    {
+      state = RESETTING;
+      display.clearScreen();
+      String message("Please place the parachute.\n");
+      PrintScreen(message);
+    }
+  }
+  
+  //if there is new accelerometer data available
+  if(imu.accelAvailable())
+  {
+    imu.readAccel();
+
+    float ax = imu.ax;
+    float ay = imu.ay;
+    float az = imu.az;
+
+    //magnitude
+    float accel = sqrt(ax * ax + ay * ay + az * az);
+
+    if (state == STANDBY)
+    {
+      if(fabs(accel - GRAVITY) > WAIT_THRESHOLD) 
+      {
+        if(millis() - startTime > 500)
+        {
+          String message("Hold still or press <return> to reset.");
+          PrintScreen(message);
+  
+          startTime = millis();
+        }
+      }
+      
+      if(millis() - startTime > WAIT_TIME)
+      {
+        state = READY;
+        display.drawRect(0,0,96,64,TSRectangleFilled,TS_8b_Green);
+
+        String message("\nDrop when ready!\n");
+        PrintScreen(message);
+      }
+    }
+    
+    else if(state == READY)
+    {
+      if(accel < FALL_THRESHOLD)
+      {
+        startTime = millis();
+        state = FREEFALL;
+
+        String message("\nFalling!\n");
+        PrintScreen(message);
+      }
+    }
+
+    else if(state == FREEFALL)
+    {
+      if(millis() - startTime > MIN_TIME)
+        state = CHUTE;
+    }
+
+    else if(state == CHUTE)
+    {
+      if(accel > LAND_THRESHOLD)
+      {
+        landTime = millis();
+        state = LANDED;
+
+        String message("\n\nLanded in ");
+        message += String((landTime - startTime) / 1000.);
+        message += " seconds!\n\n";
+        
+        PrintScreen(message);
+      }
+    }
+  }
+}
+
+int PrintScreen(String inputString)
+{
+  SerialMonitor.println(inputString);
+
+  display.clearScreen();
+  display.setFont(thinPixel7_10ptFontInfo);
+  display.setCursor(0,10);
+  display.fontColor(TS_8b_Green,TS_8b_Black);
+  display.print(inputString);
+
+  return 0;
+}
+
